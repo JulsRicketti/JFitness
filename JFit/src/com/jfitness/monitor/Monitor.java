6e793632a4510d334e0e648b154894c53dc04e3a
JulsRicketti <julsricketti@gmail.com>
2014-04-07 11:29:19 -0300

    Functions for the fuzzification process included in the Monitor abstract class

--- JFit/src/com/jfitness/monitor/Monitor.java
+++ JFit/src/com/jfitness/monitor/Monitor.java
@@ -1,15 +1,12 @@
 package com.jfitness.monitor;
 
 public abstract class Monitor {
-//test
+	
 	abstract void fuzzifier();
 	abstract void fuzzyRules();
 	abstract void inferenceEngine();
 	abstract void deffuzifier();
 	
-	void blah(){
-		
-	}
 	
 	float min(float input1, float input2){
 		if(input1<=input2)
@@ -25,4 +22,28 @@ public abstract class Monitor {
 		else
 			return input2;
 	}
+	
+	
+	float findDegreeOfMembership(float x, float m, float b){
+		return m*x +b;
+	}
+	
+	//remember: y = xm + b
+	float[] findLinearFunction(float x1, float y1, float x2, float y2){
+		float [] mAndB = new float[2];
+		mAndB[0] = findM(x1, y1, x2, y2);
+		mAndB[1] = findB(x1, y1, mAndB[0]);
+		
+		
+		return mAndB;
+	}
+	
+	float findM(float x1, float y1, float x2, float y2){
+		return (y1 - y2)/(x1-x2);
+	}
+	
+	float findB(float x, float y, float m){
+		return y - m*x;
+	}
+
 }
--- JFit/src/com/jfitness/monitor/WalkingMonitor.java
+++ JFit/src/com/jfitness/monitor/WalkingMonitor.java
@@ -1,17 +1,22 @@
 package com.jfitness.monitor;
 
+
+
 public class WalkingMonitor extends Monitor {
 
 	@Override
 	public void fuzzifier() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void timeFuzzifier(int timeInput){
+		float membershipBad, membershipAverage, membershipGood;
 		
 		if(timeInput<=15){
-			
+			membershipBad = 1;
+			membershipAverage=0;
+			membershipGood = 0;
 		}
 		if(timeInput>15 && timeInput<30){
 			
